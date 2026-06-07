efcaaf84fab501722b0eb44e4f358a9293c90f02
Milan Schoenmakers <2502547schoenmakers@zuyd.nl>
2026-06-07 17:01:33 +0200

    Classes + ViewModels

---
+++ KE03_INTDEV_SE_2_Base-main/DataAccessLayer/Models/Staff.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace DataAccessLayer.Models
+{
+	public class Staff
+	{
+		public int StaffId { get; set; }
+
+		public string FirstName { get; set; } = string.Empty;
+
+		public string LastName { get; set; } = string.Empty;
+
+		public string Email { get; set; } = string.Empty;
+
+		public int FunctionId { get; set; }
+
+		public Function? Function { get; set; }
+
+		public bool Active { get; set; }
+	}
+}
---
+++ KE03_INTDEV_SE_2_Base-main/KE03_INTDEV_SE_2_Base/ViewModels/StaffViewModel.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace KE03_INTDEV_SE_2_Base.ViewModels
+{
+	public class StaffViewModel
+	{
+		public int StaffId { get; set; }
+
+		public string FirstName { get; set; } = string.Empty;
+
+		public string LastName { get; set; } = string.Empty;
+
+		public string Email { get; set; } = string.Empty;
+
+		public int FunctionId { get; set; }
+
+		public bool Active { get; set; }
+
+		public List<SelectListItem> Functions { get; set; } = [];
+	}
+}
